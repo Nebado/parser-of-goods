e26f8c1f434218244177ebd5dc668d211d0fe410
pavel <lakrunt@gmail.com>
2020-12-04 00:45:51 +0200

    style add for table

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-/* require_once("./libs/autoload.php"); */
+require_once("./libs/autoload.php"); 
 require_once("./config.php");
-require_once("./vendor/autoload.php");
+// require_once("./vendor/autoload.php");
 require_once("./libs/CurlMulti.php");
 require_once("./libs/Curl.php");
 require_once("./libs/PhpQuery.php");
