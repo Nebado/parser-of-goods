ef2374ddb6b3aecdae70bf14d7175982341623cd
Nebado <ruslan.nbd@gmail.com>
2020-12-03 20:41:18 +0200

    Set up default bootstrap without install composer

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-/* require_once("./libs/autoload.php"); */
 require_once("./config.php");
-require_once("./vendor/autoload.php");
+require_once("./libs/autoload.php");
+/* require_once("./vendor/autoload.php"); */
 require_once("./libs/CurlMulti.php");
 require_once("./libs/Curl.php");
 require_once("./libs/PhpQuery.php");
@@ -14,6 +14,7 @@ $startTime = microtime(true);
 include_once('app/Page.php');
 
 global $time;
+
 $time = microtime(true) - $startTime;
 
 include_once('views/index.php');
