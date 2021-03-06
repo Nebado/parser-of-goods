1586db652fd528109bfac44c443ec266c1478857
Nebado <ruslan.nbd@gmail.com>
2021-03-06 11:48:59 +0200

    Add package php-debugbar

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Controllers;
 
 use App\Components\Template;
+use DebugBar\StandardDebugBar;
 
 class HomeController {
-    
+
     public function index()
     {
+        $debugbar = new StandardDebugBar();
+        $debugbarRenderer = $debugbar->getJavascriptRenderer();
+
         $template = new Template();
-        $template->render('index.html');
+        $template->render('index.html', ['debugbarRenderer' => $debugbarRenderer]);
     }
 }
